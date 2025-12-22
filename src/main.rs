78ac7c529ade7af27ac34e1a3fe92afc93e08b6f
santamn <cle.neige@gmail.com>
2025-12-22 12:29:19 +0000

    add data di 001

--- src/main.rs
+++ src/main.rs
@@ -13,19 +13,19 @@ type Real = f64; // 計算の精度を決める型
 // const STEPS: usize = 1_000_000; // シミュレーションの時間ステップ数  10^6
 // const DELTA_T: Real = 1e-8; //     時間刻み幅                10^-8
 
-// 双粒子の場合のパラメータ
+// 双粒子の場合のパラメータ(_001): 3×10^4 × 10^5 × 150 = 4.5×10^11 ステップ -> 1時間20分程度
 const PARTICLES: u64 = 30_000; //                アンサンブル平均に用いる粒子数  3×10^4
-const STEPS: usize = 100_000; //                 シミュレーションの時間ステップ数  10^5
-const TIME: Real = STEPS as Real * DELTA_T; //   総シミュレーション時間         0.001 : 短すぎる
+const STEPS: usize = 1_000_000; //               シミュレーションの時間ステップ数  10^6
+const TIME: Real = STEPS as Real * DELTA_T; //   総シミュレーション時間         0.01 : 短すぎる
 const LENGTH: Real = 0.01; //                    ディパーティクルの長さ         0.01 < チャネルの最狭部の幅 約0.038
 const DELTA_T: Real = LENGTH * LENGTH * 1e-4; // 時間刻み幅 (√δt = LENGTH/100 となるように設定)
 
 fn main() {
     let start = std::time::Instant::now();
 
-    let mut mu_writer = BufWriter::new(File::create("data/di/mu_150_001.dat").unwrap());
-    let mut d_writer = BufWriter::new(File::create("data/di/d_eff_150_001.dat").unwrap());
-    let mut alpha_writer = BufWriter::new(File::create("data/di/alpha_150_001.dat").unwrap());
+    let mut mu_writer = BufWriter::new(File::create("data/di/mu_150_01.dat").unwrap());
+    let mut d_writer = BufWriter::new(File::create("data/di/d_eff_150_01.dat").unwrap());
+    let mut alpha_writer = BufWriter::new(File::create("data/di/alpha_150_01.dat").unwrap());
 
     for i in 1..=150 {
         let f = Vector2::new(i as Real, 0.0);
